19bb56b1e45903b0d7dc25f0274be4f0f215a45d
JaviCast03 <150303311+JaviCast03@users.noreply.github.com>
2024-04-18 13:18:21 -0300

    Add files via upload

---
+++ cliente.php
@@ -0,0 +1,58 @@
+<?php 
+/*0. Se registra la siguiente información: nombre, apellido, si está o no dado de baja, el tipo y el número de
+    documento. Si un cliente está dado de baja, no puede registrar compras desde el momento de su baja.
+    1. Método constructor que recibe como parámetros los valores iniciales para los atributos.
+    2. Los métodos de acceso de cada uno de los atributos de la clase.
+    3. Redefinir el método _toString para que retorne la información de los atributos de la clase.
+
+*/
+class Cliente{
+    private $nombre;
+    private $apellido;
+    private $dadoDeBaja;
+    private $dni;
+    private $tipo;
+    public function __construct($nombre, $apellido,$dadoDeBaja,$tipo, $dni){
+        $this->nombre=$nombre;
+        $this->apellido=$apellido;
+        $this->dadoDeBaja=$dadoDeBaja;
+        $this->dni=$dni;
+        $this->tipo=$tipo;
+    }
+    //getters
+    public function getNombre(){
+        return $this->nombre;
+    }
+    public function getApellido(){
+        return $this->apellido;
+    }
+    public function getDadoDeBaja(){
+        return $this->dadoDeBaja;
+    }
+    public function getTipo(){
+        return $this->tipo;
+    }
+    public function getDni(){
+        return $this->dni;
+    }
+    //setters
+    public function setNombre($nombre){
+        $this->nombre=$nombre;
+    }
+    public function setApellido($apellido){
+        $this->apellido=$apellido;
+    }public function setDadoDeBaja($dadoDeBaja){
+        $this->dadoDeBaja=$dadoDeBaja;
+    }
+    public function setTipo($tipo){
+        $this->tipo=$tipo;
+    }
+    public function setDni($dni){
+        $this->dni=$dni;
+    }
+    public function __toString()
+    {
+        return "Nombre: ". $this->getNombre(). ", ". $this->getApellido()."\n".
+        "Esta dado de baja: ". $this->getDadoDeBaja(). "\n". "Tipo: ".$this->getTipo()."\n"."Dni: ". $this->getDni();
+    }
+}
---
+++ testEmpresa.php
@@ -0,0 +1,43 @@
+<?php
+/*1. Cree 2 instancias de la clase Cliente: $objCliente1, $objCliente2.
+    2. Cree 3 objetos Motos con la información visualizada en la tabla: código, costo, año fabricación,
+    descripción, porcentaje incremento anual, activo.
+*/
+include_once "cliente.php";
+include_once "empresa.php";
+include_once "moto.php";
+include_once "venta.php";
+function recorrerArr($arr){
+    $cad="";
+    foreach($arr as $elemento){
+        $cad= $cad." ".$elemento."\n";
+    }
+    return $cad;
+}
+$objCliente1= new Cliente("Javier", "Castillo",true,"DNI", 48);
+$objCliente2= new Cliente("Ignacio", "Castillo",false,"DNI", 32);
+$obMoto1= new Moto(11, 2230000,2022,"Benelli Imperiale 400",85,true);
+$obMoto2= new Moto(12, 584000,2021,"Zanella Zr 150 Ohc",70,true);
+$obMoto3= new Moto(13, 999900,2023,"Zanella Patagonian Eagle 250",55,false);
+$empresa= new Empresa("Alta gama","Av Argentina 123",[$objCliente1, $objCliente2],[$obMoto1, $obMoto2, $obMoto3],[]);
+$empresa->registrarVenta([11,12,13],$objCliente2);
+$ventasDeUnCliente=$empresa->retornarVentasXCliente("DNI",48);
+recorrerArr($ventasDeUnCliente);
+
+/*
+5. Invocar al método registrarVenta($colCodigosMoto, $objCliente) de la Clase Empresa donde el
+$objCliente es una referencia a la clase Cliente almacenada en la variable $objCliente2 (creada en el
+punto 1) y la colección de códigos de motos es la siguiente [11,12,13]. Visualizar el resultado obtenido.
+
+6. Invocar al método registrarVenta($colCodigosMotos, $objCliente) de la Clase Empresa donde el
+$objCliente es una referencia a la clase Cliente almacenada en la variable $objCliente2 (creada en el
+punto 1) y la colección de códigos de motos es la siguiente [0]. Visualizar el resultado obtenido.
+7. Invocar al método registrarVenta($colCodigosMotos, $objCliente) de la Clase Empresa donde el
+$objCliente es una referencia a la clase Cliente almacenada en la variable $objCliente2 (creada en el
+punto 1) y la colección de códigos de motos es la siguiente [2]. Visualizar el resultado obtenido.
+8. Invocar al método retornarVentasXCliente($tipo,$numDoc) donde el tipo y número de documento se
+corresponden con el tipo y número de documento del $objCliente1.
+9. Invocar al método retornarVentasXCliente($tipo,$numDoc) donde el tipo y número de documento se
+corresponden con el tipo y número de documento del $objCliente2
+10. Realizar un echo de la variable Empresa creada en 2.
+*/
